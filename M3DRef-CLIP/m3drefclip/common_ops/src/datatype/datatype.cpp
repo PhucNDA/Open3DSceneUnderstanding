308d0520370079043dc44a9c0c57dd060caa5af2
Nguyen Duc Anh Phuc <108280892+PhucNDA@users.noreply.github.com>
2023-10-02 02:04:32 +0700

    Delete M3DRef-CLIP directory

--- M3DRef-CLIP/m3drefclip/common_ops/src/datatype/datatype.cpp
+++
@@ -1,9 +0,0 @@
-#include "datatype.h"
-
-ConnectedComponent::ConnectedComponent(){}
-
-void ConnectedComponent::addPoint(Int pt_idx){
-	pt_idxs.push_back(pt_idx);
-}
-
-
--- M3DRef-CLIP/m3drefclip/model/module/__init__.py
+++
@@ -1,1 +0,0 @@
-from .tiny_unet import TinyUnet
